13080b77a7d08bb493646845bbd0b839504680dc
Cherry_ Cheng_x <cherrycherry@gmail.com>
2025-10-04 16:31:08 +0000

    <img src="https://functions.prod.internal.glideapps.com/getEmoji/%F0%9F%

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -16,9 +16,10 @@ export function NailIcon({ className }: { className?: string }) {
       strokeLinejoin="round"
       className={cn("w-6 h-6", className)}
     >
-      <path d="M8.5 20.5 4 16" />
-      <path d="M6.5 12.5c0-5 5-7 7-7s7 2 7 7c0 5-5 7-7 7" />
-      <path d="m14 12-2 2" />
+      <path d="M12 2a4.5 4.5 0 0 0-4.5 4.5v3.5h9V6.5A4.5 4.5 0 0 0 12 2Z" />
+      <path d="M18 12.5c0 3.31-1.69 6-3.75 6S10.5 15.81 10.5 12.5" />
+      <path d="M8.5 22.5c0-3.31 1.69-6 3.75-6s3.75 2.69 3.75 6" />
+      <path d="M12 12.5h0" />
     </svg>
   );
 }
