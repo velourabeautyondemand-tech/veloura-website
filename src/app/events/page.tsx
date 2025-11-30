9dd853da345be3f311c25c2c34768c2dc902565a
Cherry_ Cheng_x <cherrycherry@gmail.com>
2025-11-30 18:02:06 +0000

    delete apply for future opportunity

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -46,9 +46,6 @@ export default function EventsPage() {
                                     <Button asChild variant="accent">
                                         <Link href="https://forms.gle/LT6hWDMj1nKQXW9v5" target="_blank" rel="noopener noreferrer">Submit Now to Get Qualified</Link>
                                     </Button>
-                                    <Button asChild variant="outline">
-                                        <Link href="https://www.indeed.com/viewjob?jk=c35c02dd395e9fb9&from=shareddesktop_copy" target="_blank" rel="noopener noreferrer">Apply for Future Opportunities</Link>
-                                    </Button>
                                 </div>
                             </CardContent>
                         </Card>
