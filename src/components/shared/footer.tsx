f83accb86739e10523f091f0038afb146c37fe65
Huiyu Cheng <cherrycherry@gmail.com>
2025-12-08 21:54:19 +0000

    remove in in the social media

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
               <Instagram className="h-5 w-5" />
               <span className="sr-only">Instagram</span>
             </Link>
-            <Link href="https://www.linkedin.com/in/huiyu-cheng" target="_blank" rel="noopener noreferrer" className="text-muted-foreground hover:text-primary transition-colors">
+            <Link href="https://www.linkedin.com/huiyu-cheng" target="_blank" rel="noopener noreferrer" className="text-muted-foreground hover:text-primary transition-colors">
               <Linkedin className="h-5 w-5" />
               <span className="sr-only">LinkedIn</span>
             </Link>
