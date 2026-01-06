acc002ba7d6fdd0e714654198c0a37ebc1ba8a96
Huiyu Cheng <cherrycherry@gmail.com>
2026-01-06 07:01:12 +0000

    deleted or and apply on indeed

--- src/components/features/application-form.tsx
+++ src/components/features/application-form.tsx
@@ -275,23 +275,6 @@ export function ApplicationForm() {
             
             <div className="space-y-4">
                 <Button type="submit" className="w-full text-lg" size="lg">Submit Application</Button>
-                
-                <div className="relative">
-                    <div className="absolute inset-0 flex items-center">
-                        <span className="w-full border-t" />
-                    </div>
-                    <div className="relative flex justify-center text-xs uppercase">
-                        <span className="bg-card px-2 text-muted-foreground">
-                        Or
-                        </span>
-                    </div>
-                </div>
-
-                <Button asChild variant="secondary" className="w-full text-lg" size="lg">
-                    <Link href="https://www.indeed.com/viewjob?jk=c35c02dd395e9fb9&from=shareddesktop_copy" target="_blank">
-                        Apply on Indeed
-                    </Link>
-                </Button>
             </div>
             </form>
         </Form>
