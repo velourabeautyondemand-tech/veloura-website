0b405a3373d3450ebc5469f3641af7960b6a0a5a
Huiyu Cheng <cherrycherry@gmail.com>
2026-02-09 00:29:28 +0000

    post his on the event page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -27,6 +27,18 @@ export default function EventsPage() {
                              <Card className="shadow-lg overflow-hidden">
                                 <CardContent className="p-0">
                                     <Image
+                                        src="https://i.imgur.com/Rp8nDkd.jpeg"
+                                        alt="Veloura New York Event"
+                                        width={800}
+                                        height={1000}
+                                        className="w-full h-auto object-cover"
+                                        data-ai-hint="event flyer"
+                                    />
+                                </CardContent>
+                            </Card>
+                             <Card className="shadow-lg overflow-hidden">
+                                <CardContent className="p-0">
+                                    <Image
                                         src="https://i.imgur.com/cL0neWg.jpeg"
                                         alt="Veloura Event"
                                         width={800}
