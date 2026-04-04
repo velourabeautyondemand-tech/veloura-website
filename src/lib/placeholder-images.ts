04f66621018de74efbfffa6de309ad40f4624eb2
Huiyu Cheng <cherrycherry@gmail.com>
2026-04-04 03:55:31 +0000

    west palm beach picture is not showen

--- src/lib/placeholder-images.ts
+++ src/lib/placeholder-images.ts
@@ -8,4 +8,5 @@ export type ImagePlaceholder = {
   imageHint: string;
 };
 
+// Use the standard source of truth from src/app/lib/placeholder-images.json
 export const PlaceHolderImages: ImagePlaceholder[] = data.placeholderImages;
