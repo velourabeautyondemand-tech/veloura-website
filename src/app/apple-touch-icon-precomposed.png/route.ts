be53a09897b1fb9b7265e5cd57120ce38afe2b50
Huiyu Cheng <cherrycherry@gmail.com>
2026-06-16 03:04:01 +0000

    Create a Next.js API route that redirects /apple-touch-icon.png to this

---
+++ src/app/apple-touch-icon-precomposed.png/route.ts
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export async function GET() {
+  redirect('https://firebasestorage.googleapis.com/v0/b/studio-8096841563-8bcb9.firebasestorage.app/o/Veloura%20NEw%20Logo.png?alt=media&token=e5b06483-4af8-4051-a21d-704398c3966c');
+}
